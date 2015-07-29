34a38ae7b86b956a213849c681f53bb757561697
ckleban <ckleban@gmail.com>
2015-07-29 16:20:59 -0700

    more

--- make-cluster.sh
+++ make-cluster.sh
@@ -2,11 +2,18 @@
 #
 # deploy k8 cluster on clc
 
-{ ansible-playbook create-servers.yml --extra-vars "server_group=ca2-k8-qa1 location=ca2 server_count=2 password_file=/root/passwords.yml" -vvvv; } &
-{ ansible-playbook download-k8.yml --extra-vars "server_group=ca2-k8-qa1 location=ca2 server_count=2 password_file=/root/passwords.yml" -vvvv; } &
+server_group=$1
+location=$2
+server_count=$3
+password_file=$4
+working_dir=$5
+
+
+{ ansible-playbook create-servers.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5" -vvvv; } &
+{ ansible-playbook download-k8.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5" -vvvv; } &
 wait
-{ ansible-playbook change-config.yml --extra-vars "server_group=ca2-k8-qa1 location=ca2 server_count=2 password_file=/root/passwords.yml" -vvvv; } &
+{ ansible-playbook change-config.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5" -vvvv; } &
 wait
-(cd /root/ca2-k8-qa1/kubernetes/cluster && KUBERNETES_PROVIDER=ubuntu ./kube-up.sh)
+(cd $5/$1/kubernetes/cluster && KUBERNETES_PROVIDER=ubuntu ./kube-up.sh)
 
 

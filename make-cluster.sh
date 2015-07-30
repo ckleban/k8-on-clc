a655fdd793885af13d13d2dd81ec2a701e8af4b7
ckleban <ckleban@gmail.com>
2015-07-29 21:53:07 -0700

    upload a fix for reconf docker port k8 install

--- make-cluster.sh
+++ make-cluster.sh
@@ -8,10 +8,12 @@ server_count=$3
 password_file=$4
 working_dir=$5
 
-
+echo "Starting servers and downloading k8"
 { ansible-playbook create-servers.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
 { ansible-playbook download-k8.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
 wait
+
+echo "creating configuration"
 { ansible-playbook change-config.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
 wait
 
@@ -20,3 +22,9 @@ echo "Deploying Kubernetes!!!! -> cd $5/$1/kubernetes/cluster && KUBERNETES_PROV
 
 echo "Updating PATH"
 PATH=$PATH:$5/$1/kubernetes/cluster/ubuntu/binaries/
+
+echo "Fix docker on minions. Need to re-run /root/kube/reconfDocker.sh for some reason after k8 deploy"
+{ ansible-playbook fix-minions.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
+
+echo "All done. Try out your k8 cluster today! -ck"
+

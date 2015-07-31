11df31a6acd1d089300c3da83ca008c3bcd7b7af
ckleban <ckleban@gmail.com>
2015-07-31 13:14:31 -0700

    fix vars in make-cluster

--- make-cluster.sh
+++ make-cluster.sh
@@ -11,27 +11,27 @@ cpu_size=${6:-2}
 mem_size=${7:-2} 
 
 echo "Starting servers and downloading k8"
-{ ansible-playbook create-servers.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5 cpu_size=$6 mem_size=$7"; } &
-{ ansible-playbook download-k8.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
+{ ansible-playbook create-servers.yml --extra-vars "server_group=$server_group location=$location server_count=$server_count password_file=$password_file working_dir=$working_dir cpu_size=$cpu_size mem_size=$mem_size"; } &
+{ ansible-playbook download-k8.yml --extra-vars "server_group=$server_group location=$location server_count=$server_count password_file=$password_file working_dir=$working_dir"; } &
 wait
 
 echo "creating configuration"
-{ ansible-playbook change-config.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
+{ ansible-playbook change-config.yml --extra-vars "server_group=$server_group location=$location server_count=$server_count password_file=$password_file working_dir=$working_dir"; } &
 wait
 
-echo "Deploying Kubernetes!!!! -> cd $5/$1/kubernetes/cluster && KUBERNETES_PROVIDER=ubuntu ./kube-up.sh"
-(cd $5/$1/kubernetes/cluster && KUBERNETES_PROVIDER=ubuntu ./kube-up.sh)
+echo "Deploying Kubernetes!!!! -> cd $working_dir/$server_group/kubernetes/cluster && KUBERNETES_PROVIDER=ubuntu ./kube-up.sh"
+(cd $working_dir/$server_group/kubernetes/cluster && KUBERNETES_PROVIDER=ubuntu ./kube-up.sh)
 
 echo "Updating PATH"
-export PATH=$PATH:$5/$1/kubernetes/cluster/ubuntu/binaries/
+export PATH=$PATH:$working_dir/$server_group/kubernetes/cluster/ubuntu/binaries/
 
 echo "Trying addons!"
-(cd $5/$1/kubernetes/cluster/ubuntu && KUBERNETES_PROVIDER=ubuntu ./deployAddons.sh)
+(cd $working_dir/$server_group/kubernetes/cluster/ubuntu && KUBERNETES_PROVIDER=ubuntu ./deployAddons.sh)
 
 
 
 echo "Fix docker on minions. Need to re-run /root/kube/reconfDocker.sh for some reason after k8 deploy"
-{ ansible-playbook fix-minions.yml --extra-vars "server_group=$1 location=$2 server_count=$3 password_file=$4 working_dir=$5"; } &
+{ ansible-playbook fix-minions.yml --extra-vars "server_group=$server_group location=$location server_count=$server_count password_file=$password_file working_dir=$working_dir"; } &
 wait
 
 echo "All done. Try out your k8 cluster today! -ck"
